bfc87af9d5b2a24e9769d95271b927e24d71e7af
salishop <37633640+salishop@users.noreply.github.com>
2019-06-12 16:20:47 +0300

    Updates

---
+++ precache-manifest.9e8c78a5a0ba21ccbc6b87d3f91bf9a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1cdc7e29be1d06f9c475bbc1f838c02",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "f668d8be6e464637170c",
+    "url": "/tic-tac-toe/static/css/main.9a924857.chunk.css"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/tic-tac-toe/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "f668d8be6e464637170c",
+    "url": "/tic-tac-toe/static/js/main.f3e1f21c.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  }
+]);
